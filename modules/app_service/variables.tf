061ce07349512d032a66f4fb070718d5e8244b97
syedmahmoodali-techdome <syed.mahmood.ali@techdome.net.in>
2025-11-19 17:23:43 +0530

    Update variables.tf

--- modules/app_service/variables.tf
+++ modules/app_service/variables.tf
@@ -1,139 +1,68 @@
 # Basic info
 variable "clinic_name" {
-  type        = string
-  description = "Clinic name"
+  type = string
 }
 
 variable "resource_group_name" {
-  type        = string
-  description = "Resource group name"
+  type = string
 }
 
 variable "location" {
-  type        = string
-  description = "Azure region"
+  type = string
 }
 
 variable "service_plan_id" {
-  type        = string
-  description = "App Service Plan ID"
+  type = string
 }
 
-# Container image placeholders (pipeline will set actual image)
+# Docker image placeholder (pipeline overrides)
 variable "image_name" {
-  type        = string
-  description = "Docker image name (placeholder)"
-  default     = "strapi-cms"
+  type    = string
+  default = "medusa-backend"
 }
 
 variable "image_tag" {
-  type        = string
-  description = "Docker image tag (placeholder)"
-  default     = "latest"
+  type    = string
+  default = "latest"
 }
 
-# Database config
-variable "db_host" {
-  type        = string
-  description = "Database host / FQDN"
-}
-
-variable "db_port" {
-  type        = number
-  description = "Database port"
-  default     = 5432
-}
-
-variable "db_name" {
-  type        = string
-  description = "Database name"
-}
-
-variable "db_user" {
-  type        = string
-  description = "Database username"
-}
-
-variable "db_password" {
-  type        = string
-  description = "Database password"
-  sensitive   = true
-}
-
-# Strapi admin
-variable "strapi_admin_email" {
-  type        = string
-}
-
-variable "strapi_admin_password" {
+# Medusa DB
+variable "database_url" {
   type        = string
   sensitive   = true
+  description = "Full postgres URL"
 }
 
-# Secrets (passed from root randoms)
-variable "app_keys" {
-  type        = string
-  sensitive   = true
-}
-
-variable "api_token_salt" {
-  type        = string
-  sensitive   = true
-}
-
-variable "admin_jwt_secret" {
-  type        = string
-  sensitive   = true
-}
-
-variable "transfer_token_salt" {
-  type        = string
-  sensitive   = true
-}
-
+# Medusa Secrets
 variable "jwt_secret" {
-  description = "Strapi JWT secret for user authentication"
-  type        = string
-  sensitive   = true
+  type      = string
+  sensitive = true
 }
 
+variable "cookie_secret" {
+  type      = string
+  sensitive = true
+}
 
-# Optional fields
-variable "linked_storefront_url" {
+# Optional
+variable "redis_url" {
+  type      = string
+  default   = ""
+  sensitive = true
+}
+
+variable "public_backend_url" {
   type    = string
   default = ""
 }
 
-variable "backend_url" {
-  type    = string
-  default = ""
-}
-
+# Metadata
 variable "github_token" {
   type      = string
   sensitive = true
   default   = ""
 }
 
-# Branding
-variable "brand_primary_color" {
-  type    = string
-  default = ""
-}
-variable "brand_secondary_color" {
-  type    = string
-  default = ""
-}
-variable "brand_logo_url" {
-  type    = string
-  default = ""
-}
-variable "brand_favicon_url" {
-  type    = string
-  default = ""
-}
-
-# Metadata
 variable "plan_sku" { type = string }
 variable "repo_url" { type = string }
 variable "repo_branch" { type = string }
