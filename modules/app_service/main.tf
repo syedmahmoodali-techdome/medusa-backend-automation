a9ebf01af67def4c81235ddf0b1bed54f4c63e04
syedmahmoodali-techdome <syed.mahmood.ali@techdome.net.in>
2025-11-19 16:37:10 +0530

    Update main.tf

--- modules/app_service/main.tf
+++ modules/app_service/main.tf
@@ -1,3 +1,8 @@
+provider "azurerm" {
+  features {}
+}
+
+# Container Registry (ACR)
 resource "azurerm_container_registry" "acr" {
   name                = "${replace(lower(var.clinic_name), "-", "")}acr"
   resource_group_name = var.resource_group_name
@@ -6,6 +11,7 @@ resource "azurerm_container_registry" "acr" {
   admin_enabled       = true
 }
 
+# Linux Web App
 resource "azurerm_linux_web_app" "app_service" {
   name                = "${lower(var.clinic_name)}-app"
   resource_group_name = var.resource_group_name
@@ -14,7 +20,7 @@ resource "azurerm_linux_web_app" "app_service" {
 
   site_config {
     always_on = true
-
+    # NOTE: we do NOT set linux_fx_version or container_registry here so pipeline (az cli) can manage the image
     application_stack {
       docker_image_name   = "${var.image_name}:${var.image_tag}"
       docker_registry_url = "https://${azurerm_container_registry.acr.login_server}"
@@ -22,15 +28,34 @@ resource "azurerm_linux_web_app" "app_service" {
   }
 
   app_settings = {
-    WEBSITES_PORT = "9000"
+    WEBSITES_PORT                        = "1337"
+    WEBSITES_ENABLE_APP_SERVICE_STORAGE  = "false"
 
-    # Medusa + DB
-    DATABASE_URL = var.database_url
-    JWT_SECRET   = var.jwt_secret
-    COOKIE_SECRET = var.cookie_secret
-    NODE_ENV = "production"
+    # DB
+    DATABASE_CLIENT      = "postgres"
+    DATABASE_HOST        = var.db_host
+    DATABASE_PORT        = tostring(var.db_port)
+    DATABASE_NAME        = var.db_name
+    DATABASE_USERNAME    = var.db_user
+    DATABASE_PASSWORD    = var.db_password
 
-    # Optional branding
+    # i added these two to fix the error: Cannot send secure cookie over unencrypted connection
+    TRUST_PROXY                 = "true"
+    ADMIN_FORCE_SECURE_COOKIES  = "false"
+    NODE_ENV                        = "production"
+
+    # Strapi admin
+    STRAPI_ADMIN_EMAIL    = var.strapi_admin_email
+    STRAPI_ADMIN_PASSWORD = var.strapi_admin_password
+    
+
+    # Strapi secrets
+    APP_KEYS             = var.app_keys
+    API_TOKEN_SALT       = var.api_token_salt
+    ADMIN_JWT_SECRET     = var.admin_jwt_secret
+    TRANSFER_TOKEN_SALT  = var.transfer_token_salt
+
+    # Optional integrations & branding
     LINKED_STOREFRONT_URL = var.linked_storefront_url
     BACKEND_URL           = var.backend_url
 
@@ -38,14 +63,11 @@ resource "azurerm_linux_web_app" "app_service" {
     BRAND_SECONDARY_COLOR = var.brand_secondary_color
     BRAND_LOGO_URL        = var.brand_logo_url
     BRAND_FAVICON_URL     = var.brand_favicon_url
-
-    # ACR auth
-    DOCKER_REGISTRY_SERVER_URL      = "https://${azurerm_container_registry.acr.login_server}"
-    DOCKER_REGISTRY_SERVER_USERNAME = azurerm_container_registry.acr.admin_username
-    DOCKER_REGISTRY_SERVER_PASSWORD = azurerm_container_registry.acr.admin_password
+    JWT_SECRET = var.jwt_secret
   }
 }
 
+# Outputs
 output "app_service_name" {
   value = azurerm_linux_web_app.app_service.name
 }
@@ -60,4 +82,15 @@ output "acr_name" {
 
 output "acr_login_server" {
   value = azurerm_container_registry.acr.login_server
+}
+
+# Provide ACR admin credentials for pipeline convenience
+output "acr_admin_username" {
+  value     = azurerm_container_registry.acr.admin_username
+  sensitive = true
+}
+
+output "acr_admin_password" {
+  value     = azurerm_container_registry.acr.admin_password
+  sensitive = true
 }
