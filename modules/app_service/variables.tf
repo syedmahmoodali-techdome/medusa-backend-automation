3f277fedfc8e9988189791b6aecf037fa3aa0ab6
syedmahmoodali-techdome <syed.mahmood.ali@techdome.net.in>
2025-11-19 16:01:17 +0530

    Update variables.tf

--- modules/app_service/variables.tf
+++ modules/app_service/variables.tf
@@ -1,142 +1,94 @@
-# Basic info
-variable "clinic_name" {
-  type        = string
-  description = "Clinic name"
+variable "service_plan_id" {
+  type = string
 }
 
 variable "resource_group_name" {
-  type        = string
-  description = "Resource group name"
+  type = string
 }
 
 variable "location" {
-  type        = string
-  description = "Azure region"
+  type = string
 }
 
-variable "service_plan_id" {
-  type        = string
-  description = "App Service Plan ID"
+variable "clinic_name" {
+  type = string
 }
 
-variable "database_url" {}
-variable "cookie_secret" {}
-
-# Container image placeholders (pipeline will set actual image)
+# Docker image
 variable "image_name" {
-  type        = string
-  description = "Docker image name (placeholder)"
-  default     = "strapi-cms"
+  type = string
 }
 
 variable "image_tag" {
-  type        = string
-  description = "Docker image tag (placeholder)"
-  default     = "latest"
+  type = string
 }
 
-# Database config
+# Database
 variable "db_host" {
-  type        = string
-  description = "Database host / FQDN"
+  type = string
 }
 
 variable "db_port" {
-  type        = number
-  description = "Database port"
-  default     = 5432
+  type = number
 }
 
 variable "db_name" {
-  type        = string
-  description = "Database name"
+  type = string
 }
 
 variable "db_user" {
-  type        = string
-  description = "Database username"
+  type = string
 }
 
 variable "db_password" {
-  type        = string
-  description = "Database password"
-  sensitive   = true
+  type = string
 }
 
-# Strapi admin
-variable "strapi_admin_email" {
-  type        = string
+variable "database_url" {
+  type = string
 }
 
-variable "strapi_admin_password" {
-  type        = string
-  sensitive   = true
-}
-
-# Secrets (passed from root randoms)
-variable "app_keys" {
-  type        = string
-  sensitive   = true
-}
-
-variable "api_token_salt" {
-  type        = string
-  sensitive   = true
-}
-
-variable "admin_jwt_secret" {
-  type        = string
-  sensitive   = true
-}
-
-variable "transfer_token_salt" {
-  type        = string
-  sensitive   = true
-}
-
+# Secrets
 variable "jwt_secret" {
-  description = "Strapi JWT secret for user authentication"
-  type        = string
-  sensitive   = true
+  type = string
 }
 
+variable "cookie_secret" {
+  type = string
+}
 
-# Optional fields
+# Optional branding
 variable "linked_storefront_url" {
-  type    = string
-  default = ""
+  type      = string
+  default   = null
 }
 
 variable "backend_url" {
-  type    = string
-  default = ""
-}
-
-variable "github_token" {
   type      = string
-  sensitive = true
-  default   = ""
+  default   = null
 }
 
-# Branding
 variable "brand_primary_color" {
-  type    = string
-  default = ""
-}
-variable "brand_secondary_color" {
-  type    = string
-  default = ""
-}
-variable "brand_logo_url" {
-  type    = string
-  default = ""
-}
-variable "brand_favicon_url" {
-  type    = string
-  default = ""
+  type      = string
+  default   = null
 }
 
-# Metadata
-variable "plan_sku" { type = string }
-variable "repo_url" { type = string }
-variable "repo_branch" { type = string }
+variable "brand_secondary_color" {
+  type      = string
+  default   = null
+}
+
+variable "brand_logo_url" {
+  type      = string
+  default   = null
+}
+
+variable "brand_favicon_url" {
+  type      = string
+  default   = null
+}
+
+# GitHub token (pipeline)
+variable "github_token" {
+  type = string
+}
