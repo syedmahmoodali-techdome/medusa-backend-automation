0f4c98020798eda6557b436128b479cc41556b15
syedmahmoodali-techdome <syed.mahmood.ali@techdome.net.in>
2025-11-19 17:26:44 +0530

    Update variables.tf

--- modules/app_service/variables.tf
+++ modules/app_service/variables.tf
@@ -1,50 +1,16 @@
-# Basic info
-variable "clinic_name" {
-  type = string
-}
-
-variable "resource_group_name" {
-  type = string
-}
-
-variable "location" {
-  type = string
-}
-
-variable "service_plan_id" {
-  type = string
-}
-
-# Docker image placeholder (pipeline overrides)
-variable "image_name" {
-  type    = string
-  default = "medusa-backend"
-}
-
-variable "image_tag" {
-  type    = string
-  default = "latest"
-}
-
-# Medusa DB
+# -----------------------
+# Medusa values (new)
+# -----------------------
 variable "database_url" {
   type        = string
   sensitive   = true
-  description = "Full postgres URL"
-}
-
-# Medusa Secrets
-variable "jwt_secret" {
-  type      = string
-  sensitive = true
 }
 
 variable "cookie_secret" {
-  type      = string
-  sensitive = true
+  type        = string
+  sensitive   = true
 }
 
-# Optional
 variable "redis_url" {
   type      = string
   default   = ""
@@ -55,14 +21,3 @@ variable "public_backend_url" {
   type    = string
   default = ""
 }
-
-# Metadata
-variable "github_token" {
-  type      = string
-  sensitive = true
-  default   = ""
-}
-
-variable "plan_sku" { type = string }
-variable "repo_url" { type = string }
-variable "repo_branch" { type = string }
