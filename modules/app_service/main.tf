a47916807050915f76cc4949c06a57a7606d2ae8
syedmahmoodali-techdome <syed.mahmood.ali@techdome.net.in>
2025-11-24 12:38:46 +0530

    Update main.tf

--- modules/app_service/main.tf
+++ modules/app_service/main.tf
@@ -74,21 +74,21 @@ output "cms_url" {
   value = azurerm_linux_web_app.app_service.default_hostname
 }
 
-output "acr_name" {
-  value = azurerm_container_registry.acr.name
-}
+#output "acr_name" {
+#  value = azurerm_container_registry.acr.name
+#}
 
-output "acr_login_server" {
-  value = azurerm_container_registry.acr.login_server
-}
+#output "acr_login_server" {
+#  value = azurerm_container_registry.acr.login_server
+#}
 
 # Provide ACR admin credentials for pipeline convenience
-output "acr_admin_username" {
-  value     = azurerm_container_registry.acr.admin_username
-  sensitive = true
-}
+#output "acr_admin_username" {
+#  value     = azurerm_container_registry.acr.admin_username
+#  sensitive = true
+#}
 
-output "acr_admin_password" {
-  value     = azurerm_container_registry.acr.admin_password
-  sensitive = true
-}
+#output "acr_admin_password" {
+#  value     = azurerm_container_registry.acr.admin_password
+#  sensitive = true
+#}
